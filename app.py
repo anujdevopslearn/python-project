d3e9fe88dadfb8dc0cf7446a5f30be0405fb4b5b
Maheshdonthala <maheshdonthala423@gmail.com>
2025-08-28 13:18:52 +0530

    By MB

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ import streamlit as st
 
 st.title("Sleep Predictor AI 🧠")
 
-# Input from user
+
 hours = st.slider("How many hours did you sleep?", 0, 12, 6)
 
-# Basic ML-like logic
 def predict(hours_slept):
     return "Tired 😴" if hours_slept < 6 else "Not Tired 😃"
 
-# Show prediction
 prediction = predict(hours)
 st.write(f"Prediction: **{prediction}**")
 
 
 api_key = st.secrets["API_KEY"]
 
-# Fetch another secret
 db_password = st.secrets["DB_PASSWORD"]
 
 st.write("✅ Secrets fetched successfully!")
